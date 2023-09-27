bd56d9e1e57949baf0c69c20a113ad58a290d499
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-09-27 20:53:33 +0300

    update
    fix Profile update backend

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,11 +1,23 @@
 import { Container, Title, StyledImage } from './FirstPage.styled';
 import example from '../../assets/example.png';
+import React from 'react';
+import CreatableSelect from 'react-select/creatable';
+import {Sel} from './Sel';
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
+
 
 const FirstPage = () => {
   return (
     <Container>
       <Title>First Page</Title>
       <StyledImage src={example} alt="Example" />
+      <Sel />
     </Container>
   );
 };
